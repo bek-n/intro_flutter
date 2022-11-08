9bac222a22783b28ebd616d3e8ab8746158fc68d
Bek <nematjanov95@gmail.com>
2022-11-08 19:19:37 +0500

    ichma ich

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,55 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Container(
-      color: Colors.blue,
+      color: Colors.green,
       child: Center(
         child: Stack(
           children: [
-            Align(
-              alignment: Alignment.center,
+            Positioned(
+              left: 300,
               child: Container(
                 height: 4000,
-                width: 100,
+                width: 200,
                 color: Colors.yellow,
               ),
             ),
             Positioned(
-              bottom: 500,
+              top: 850,
               right: 0,
               child: Container(
-                height: 100,
-                width: 700,
+                height: 500,
+                width: 1000,
                 color: Colors.yellow,
+              ),
+            ),
+            Positioned(
+              left: 20,
+              top: 200,
+              right: 150,
+              child: Container(
+                height: 500,
+                width: 200,
+                color: Colors.yellow,
+              ),
+            ),
+            Positioned(
+              left: 80,
+              top: 230,
+              right: 200,
+              child: Container(
+                height: 320,
+                width: 0,
+                color: Colors.blue,
+              ),
+            ),
+            Positioned(
+              left: 120,
+              top: 240,
+              right: 240,
+              child: Container(
+                height: 200,
+                width: 3,
+                color: Colors.green,
               ),
             )
           ],
