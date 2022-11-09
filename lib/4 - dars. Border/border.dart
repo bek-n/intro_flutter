ede7c525b414f30debfc93bb10ef6cdc9087dc51
Bek <nematjanov95@gmail.com>
2022-11-09 17:54:18 +0500

    rectangle with border hello world

--- lib/4 - dars. Border/border.dart
+++ lib/4 - dars. Border/border.dart
@@ -7,20 +7,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Container(
-      color: Colors.black,
-      child: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-              color: Colors.red,
-              border: Border(
-                  bottom: BorderSide(color: Colors.purple, width: 10),
-                  top: BorderSide(color: Colors.cyan, width: 10),
-                  right: BorderSide(color: Colors.teal, width: 10),
-                  left: BorderSide(color: Colors.orange, width: 10))),
-        ),
-      ),
-    ));
+            color: Colors.white,
+            child: Center(
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Colors.yellow,
+                    borderRadius: BorderRadius.all(Radius.circular(32)),
+                    border: Border.all(color: Colors.black, width: 10)),
+                child: Center(
+                  child: Text(
+                    'Hello World',
+                    style: TextStyle(
+                        decoration: TextDecoration.none, fontSize: 20),
+                  ),
+                ),
+              ),
+            )));
   }
 }
